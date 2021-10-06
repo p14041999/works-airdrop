d94c1e8386bd48a47cf4ad601aadff3e0385e801
p14041999 <p14041999@gmail.com>
2021-10-07 00:48:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBIyevYcNlKJFSLZdX4y9OkIbNWSnLI1po",
-    authDomain: "works-airdrop-73454.firebaseapp.com",
-    projectId: "works-airdrop-73454",
-    storageBucket: "works-airdrop-73454.appspot.com",
-    messagingSenderId: "959907568526",
-    appId: "1:959907568526:web:1256cc9b98325773c8e2df",
-    measurementId: "G-V2EHKCPHSH"
+  apiKey: "AIzaSyCl2ZL7tX9BGMHK18m3PNjRaMMY-AeRN_s",
+  authDomain: "works-52666.firebaseapp.com",
+  projectId: "works-52666",
+  storageBucket: "works-52666.appspot.com",
+  messagingSenderId: "811896829814",
+  appId: "1:811896829814:web:3efcd91ca06aca4eabec57",
+  measurementId: "G-VBRD2RVXST"
 };
